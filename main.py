7e15f02503e37440c722b04e94ee0e7f653a6908
skymiku39 <52848246+skymiku39@users.noreply.github.com>
2021-03-24 23:35:51 +0800

    feat✨(main,openCV_test): 練習 function() 使用變數作為檔案路徑

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import openCV_test as opencv
 def main():
 
     # opencv.show_main_explorer()
-    opencv.show_pic_explorer_pic()
-    
+    # opencv.show_pic_explorer_pic()
+    key_in_file_name = input("請輸入檔名，預設image.jpg")
+    opencv.key_in_variable(key_in_file_name)
+
 
 if __name__ == "__main__":
 
--- openCV_test.py
+++ openCV_test.py
@@ -15,3 +15,14 @@ def show_pic_explorer_pic():  # 指定目錄並顯示圖片
     cv2.waitKey(0)  # 等待按鍵
     cv2.destroyAllWindows()  # 關閉視窗，不加也可使用
 
+
+def key_in_variable(name):  # 使用輸入的變數作為影像路徑
+    print("輸入字串 " + name)
+    if name == "":  # 如果沒有輸入字元，會用image.jpg取代
+        name = "image.jpg"
+
+    img = cv2.imread(name)  # 圖片位置
+    cv2.imshow("key_in_variable", img)  # 顯示圖片
+    cv2.waitKey(0)  # 等待按鍵
+    cv2.destroyAllWindows()  # 關閉視窗，不加也可使用
+
