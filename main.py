6fdbb95e33543bf5f64f5b4cb474201ac9867fbf
skymiku39 <52848246+skymiku39@users.noreply.github.com>
2021-03-24 23:32:44 +0800

    feat✨(main,openCV_test): 練習使用 openCV 指定檔案目錄的方式顯示圖片

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import openCV_test as opencv
 
 def main():
 
-    opencv.show_main_explorer()
+    # opencv.show_main_explorer()
+    opencv.show_pic_explorer_pic()
+    
 
 if __name__ == "__main__":
 
--- openCV_test.py
+++ openCV_test.py
@@ -7,3 +7,11 @@ def show_main_explorer():  # 載入並顯示圖片
     cv2.waitKey(0)  # 等待按鍵
     cv2.destroyAllWindows()  # 關閉視窗，不加也可使用
 
+
+def show_pic_explorer_pic():  # 指定目錄並顯示圖片
+    file_name = "./pic/imageMiku.jpg"  # 指定目錄
+    img = cv2.imread(file_name)  # 圖片位置
+    cv2.imshow("show_pic_explorer_pic", img)  # 顯示圖片
+    cv2.waitKey(0)  # 等待按鍵
+    cv2.destroyAllWindows()  # 關閉視窗，不加也可使用
+
