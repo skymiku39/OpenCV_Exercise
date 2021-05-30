63198ff0c91bff46ac6cf5a4c1cbbfa2e397e862
skymiku39 <52848246+skymiku39@users.noreply.github.com>
2021-05-31 03:27:50 +0800

    feat✨(main,openCV_test): 練習旋轉圖片

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ import openCV_test as opencv
 
 
 def main():
-
     # opencv.show_main_explorer()
     # opencv.show_pic_explorer_pic()
-    key_in_file_name = input("請輸入檔名，預設image.jpg")
-    opencv.key_in_variable(key_in_file_name)
+    #
+    # key_in_file_name = input("請輸入檔名，預設image.jpg")
+    # opencv.key_in_variable(key_in_file_name)
+    #
+
+    key_in_angle = input("請輸入旋轉角度，預設15度 -順時針/+逆時針")
+    opencv.key_in_angle(key_in_angle)
 
 
 if __name__ == "__main__":
-
     main()
--- openCV_test.py
+++ openCV_test.py
@@ -1,4 +1,5 @@
 import cv2
+import matplotlib.pyplot as plt  # 數值數學擴展包 NumPy的可視化操作界面
 
 
 def show_main_explorer():  # 載入並顯示圖片
@@ -26,3 +27,28 @@ def key_in_variable(name):  # 使用輸入的變數作為影像路徑
     cv2.waitKey(0)  # 等待按鍵
     cv2.destroyAllWindows()  # 關閉視窗，不加也可使用
 
+
+def show_img(img):  # 用 matplotlib 顯示圖片
+
+    image_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # cv2 轉成 matplotlib 格式
+    plt.imshow(image_rgb)
+    plt.show()
+
+
+def key_in_angle(angle):  # 使用輸入的變數作為影像路徑
+    file_name = "image.jpg"
+    img = cv2.imread(file_name)  # 圖片位置
+    print("輸入字串 " + angle)
+    if angle == "":  # 如果沒有輸入字元，會用image.jpg取代
+        angle = 15
+
+    angle = int(angle)  # 字串轉數字
+    (h, w, d) = img.shape
+    center = (w // 2, h // 2)
+
+    # 第一個參數旋轉中心，第二個參數旋轉角度(-順時針/+逆時針)，第三個參數縮放比例
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+
+    # 第三個參數變化後的圖片大小
+    rotate_img = cv2.warpAffine(img, M, (w, h))
+    show_img(rotate_img)
